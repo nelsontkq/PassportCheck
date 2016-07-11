f555482f7a1e14309b3cdf28272af99061c10c46
Nelson <truran@outlook.com>
2016-07-11 18:57:14 +0100

    added main content

---
+++ src/passportcheck/ChecksumCalculator.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package passportcheck;
+
+/**
+ *
+ * @author ntko
+ */
+public class ChecksumCalculator {
+    public int Checksum(int input) {
+        int checksum = 0;
+        
+        return checksum;
+    }
+}
